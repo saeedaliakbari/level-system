a7559c54d95885978227d5478fa4efedca8c3d71
Saeed Aliakbari <saliakbari6603@gmail.com>
2023-07-19 10:12:59 +0330

    login process and in signup check email not duplicate

--- controller/auth.js
+++ controller/auth.js
@@ -5,14 +5,53 @@ exports.signup = (req, res, next) => {
   const name = req.body.name;
   const email = req.body.email;
   const password = req.body.password;
-  const user = new User({ name, email, password });
-  user
-    .save()
+  User.findOne({ email: email })
+    .then((user) => {
+      if (!user) {
+        const user = new User({ name, email, password });
+        return user.save();
+      } else {
+        const error = new Error("email already exsits ");
+        throw error;
+      }
+    })
     .then((result) => {
       res.status(401).json({ message: "User created", userId: result._id });
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
 exports.login = (req, res, next) => {
   console.log("login request");
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email: email })
+    .then((user) => {
+      if (!user) {
+        const error = new Error("user not found");
+        error.statusCode = 401;
+        throw error;
+      }
+      console.log(user);
+      if (user.password === password) {
+        res.status(200).json({
+          message: "login success",
+          userId: loadedUser._id.toString(),
+        });
+      } else {
+        const error = new Error("password is wrong");
+        error.statusCode = 401;
+        throw error;
+      }
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
